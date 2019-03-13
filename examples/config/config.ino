6136dfae370db954bc3a64f4a70c9b6b0d9d75c7
forKnowYou <1003425554@qq.com>
2019-03-13 11:21:15 +0800

    fix bug

--- examples/config/config.ino
+++ examples/config/config.ino
@@ -14,20 +14,20 @@
 #include "DFRobot_BMP280.h"
 #include "Wire.h"
 
-typedef DFRobot_BMP280_IIC    BME;    // ******** use abbreviations instead of full names ********
+typedef DFRobot_BMP280_IIC    BMP;    // ******** use abbreviations instead of full names ********
 
-BME   bmp(&Wire, BME::eSdo_low);
+BMP   bmp(&Wire, BMP::eSdo_low);
 
 #define SEA_LEVEL_PRESSURE    1015.0f   // sea level pressure
 
 // show last sensor operate status
-void printLastOperateStatus(BME::eStatus_t eStatus)
+void printLastOperateStatus(BMP::eStatus_t eStatus)
 {
   switch(eStatus) {
-  case BME::eStatusOK:    Serial.println("everything ok"); break;
-  case BME::eStatusErr:   Serial.println("unknow error"); break;
-  case BME::eStatusErrDeviceNotDetected:    Serial.println("device not detected"); break;
-  case BME::eStatusErrParameter:    Serial.println("parameter error"); break;
+  case BMP::eStatusOK:    Serial.println("everything ok"); break;
+  case BMP::eStatusErr:   Serial.println("unknow error"); break;
+  case BMP::eStatusErrDeviceNotDetected:    Serial.println("device not detected"); break;
+  case BMP::eStatusErrParameter:    Serial.println("parameter error"); break;
   default: Serial.println("unknow status"); break;
   }
 }
@@ -37,18 +37,18 @@ void setup()
   Serial.begin(115200);
   bmp.reset();
   Serial.println("bmp config test");
-  while(bmp.begin() != BME::eStatusOK) {
+  while(bmp.begin() != BMP::eStatusOK) {
     Serial.println("bmp begin faild");
     printLastOperateStatus(bmp.lastOperateStatus);
     delay(2000);
   }
   Serial.println("bmp begin success");
 
-  bmp.setConfigFilter(BME::eConfigFilter_off);        // set config filter
-  bmp.setConfigTStandby(BME::eConfigTStandby_125);    // set standby time
-  bmp.setCtrlMeasSamplingTemp(BME::eSampling_X8);     // set temperature over sampling
-  bmp.setCtrlMeasSamplingPress(BME::eSampling_X8);    // set pressure over sampling
-  bmp.setCtrlMeasMode(BME::eCtrlMeasMode_normal);     // set control measurement mode to make these settings effective
+  bmp.setConfigFilter(BMP::eConfigFilter_off);        // set config filter
+  bmp.setConfigTStandby(BMP::eConfigTStandby_125);    // set standby time
+  bmp.setCtrlMeasSamplingTemp(BMP::eSampling_X8);     // set temperature over sampling
+  bmp.setCtrlMeasSamplingPress(BMP::eSampling_X8);    // set pressure over sampling
+  bmp.setCtrlMeasMode(BMP::eCtrlMeasMode_normal);     // set control measurement mode to make these settings effective
 
   delay(100);
 }
